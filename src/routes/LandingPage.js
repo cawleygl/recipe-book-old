72ad0657b211b8ca8b1c4e5cfcc42cbc6a882151
Grant Cawley <grantlcawley@gmail.com>
2022-03-13 21:06:07 -0400

    Pass in recipes

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -1,9 +1,12 @@
 import RecipeDisplay from "../components/RecipeDisplay";
+import { getRecipes } from "../data";
 
+let recipes = getRecipes();
 
 const LandingPage = () => {
   return (
     <RecipeDisplay
+      recipes={recipes}
       ingredientToggle={true}
       directionToggle={true}
       nutritionToggle={true}
