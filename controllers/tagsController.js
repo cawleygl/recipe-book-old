747d0937bff4cdfbc85c7066884b3ac9c44bf89d
Grant Cawley <grantlcawley@gmail.com>
2022-04-19 00:17:13 -0400

    sort tags

--- controllers/tagsController.js
+++ controllers/tagsController.js
@@ -5,7 +5,7 @@ module.exports = {
   findAll: function(req, res) {
     db.Tag
       .find(req.query)
-      .sort({ date: -1 })
+      .sort({ name: "asc" })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
